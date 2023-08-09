dfa36c87db68854e1e0186678a8c88e805863401
Abdulla Abdurakhmanov <me@abdolence.dev>
2023-08-09 12:05:23 +0200

    AWS SDK compatibility fix

--- src/errors.rs
+++ src/errors.rs
@@ -126,10 +126,10 @@ impl From<tonic::Status> for KmsAeadError {
 }
 
 #[cfg(feature = "aws")]
-impl<E: Display + Error + Sync + Send + 'static> From<aws_sdk_kms::error::SdkError<E>>
-    for KmsAeadError
+impl<E: Display + Error + Sync + Send + 'static, R: std::fmt::Debug + Sync + Send + 'static>
+    From<aws_sdk_kms::error::SdkError<E, R>> for KmsAeadError
 {
-    fn from(e: aws_sdk_kms::error::SdkError<E>) -> Self {
+    fn from(e: aws_sdk_kms::error::SdkError<E, R>) -> Self {
         KmsAeadError::EncryptionError(
             KmsAeadEncryptionError::new(
                 KmsAeadErrorPublicGenericDetails::new(format!("{e}")),
